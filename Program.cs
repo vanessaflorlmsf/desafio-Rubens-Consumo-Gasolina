3e55dbc65ab3beb9be663f9f48226eed4da29475
vanessa <vanessaalves489@gmail.com>
2021-02-10 19:20:53 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class ProgramCalcular
+    {
+    static void Main(string[] args)
+        {
+     int distancia;
+     double consumo;
+
+     Console.WriteLine("Informe a distancia a percorrer");
+     distancia = int.Parse(Console.ReadLine());
+     consumo = distancia/12:00;
+     Console.WriteLine("A quantidade de combustivel é{0:N}",consumo);
+     
+
+    }
+  }
+}
